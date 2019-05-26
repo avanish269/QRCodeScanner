44b0bc33c0920625f57dc943a1f6bbdfead2e570
avanish269 <avanish269@gmail.com>
2019-05-26 18:18:26 +0530

    Final Scanner

--- src/js/qrscanner.js
+++ src/js/qrscanner.js
@@ -8,6 +8,26 @@ var app=new Vue({
   },
   mounted:function(){
     var self=this;
+    var isMobile={
+      Android: function(){
+        return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function(){
+        return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function(){
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function(){
+        return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+    };
     self.scanner=new Instascan.Scanner({video:document.getElementById("pre"),scanPeriod:5});
     self.scanner.addListener('scan',function(content,image){
       self.scans.unshift({date:+(Date.now()), content:content});
@@ -15,8 +35,12 @@ var app=new Vue({
     Instascan.Camera.getCameras().then(function(cameras){
       self.cameras=cameras;
       if(cameras.length>0){
-        self.activeCameraId=cameras[0].id;
-        self.scanner.start(cameras[0]);
+        var i=0;
+        if(isMobile.Android() || isMobile.iOS()){
+          i=1;
+        }
+        self.activeCameraId=cameras[i].id;
+        self.scanner.start(cameras[i]);
       }
       else{
         console.error('No cameras');
