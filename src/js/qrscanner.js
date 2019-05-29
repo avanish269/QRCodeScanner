a4cccc5cac1d6d1c888ece8383ba025ce82c8b44
avanish269 <avanish269@gmail.com>
2019-05-30 03:47:59 +0530

    Ready to be used

--- src/js/qrscanner.js
+++ src/js/qrscanner.js
@@ -28,9 +28,44 @@ var app=new Vue({
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
       }
     };
-    self.scanner=new Instascan.Scanner({video:document.getElementById("pre"),scanPeriod:5});
+    var qrcontent="q";
+    var x=123;
+    var count=0;
+    self.scanner=new Instascan.Scanner({video:document.getElementById("pre"),scanPeriod:1});
     self.scanner.addListener('scan',function(content,image){
       self.scans.unshift({date:+(Date.now()), content:content});
+      qrcontent=content;
+      if(!isNaN(qrcontent)){
+        x=parseInt(qrcontent);
+      }
+      else{
+        x=60606060;
+      }
+      if(isNaN(x)){
+        alert("Invalid Data! Scan another qr code");
+      }
+      else{
+        x1=x%100;
+        y=Math.floor(x/100);
+        x2=y%100;
+        y=Math.floor(y/100);
+        x3=y%100;
+        y=Math.floor(y/100);
+        x4=y%100;
+        y=Math.floor(y/100);
+        if((x1>=0&&x1<=59)&&(x2>=0&&x2<=59)&&(x3>=0&&x3<=59)&&(x4>=0&&x4<=59)&&(y==0)){
+          alert(x1);
+          alert(x2);
+          alert(x3);
+          alert(x4);
+          alert("Valid Data");
+          self.scanner.stop();
+          location.href="https://www.google.com";
+        }
+        else{
+          alert("Invalid Data! Scan another qr code");
+        }
+      }
     });
     Instascan.Camera.getCameras().then(function(cameras){
       self.cameras=cameras;
